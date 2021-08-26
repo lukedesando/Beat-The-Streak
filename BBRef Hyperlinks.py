4126c41082c866b541c2a0c50223da9cde0b9214
lukedesando <Lukedesando@gmail.com>
2021-08-26 03:49:44 -0400

    Configuring Hyperlinks for BBRef Pages

--- BBRef Hyperlinks.py
+++ BBRef Hyperlinks.py
@@ -1,47 +1,32 @@
 from baseball_scraper import espn, playerid_lookup
-from BackgroundFunctions import Find_Fangraph_ID, num_names
+from BackgroundFunctions import Find_Fangraph_ID, num_names,Get_BBRef_ID
 
 #FIXME It's all here, just needs to be cleaned up into a function
 
-player_name = "Trea Turner"
+PlayerName = "Fernando Tatis"
+batting_or_pitching = "batting"
 #TODO Need Player_Type to be automatically configured as p for pitcher or b for batter, otherwise it'll take forever
-batter_or_pitcher = "batter"
-b_or_p = "b"
-bat_or_pitch = "bat"
-Year = 2020
+if batting_or_pitching == "pitching":
+    b_or_p = "p"
+    bat_or_pitch = "pitch"
+else:
+    b_or_p = "b"
+    bat_or_pitch = "bat"
 
+Year = 2021
 
-PlayerFirstName,PlayerLastName=player_name.split(" ")
-Player_ID_Dataframe = playerid_lookup(PlayerLastName,PlayerFirstName)
+#Referenced from Background Functions
+BBRefID = Get_BBRef_ID(PlayerName)
+BBRefLetter = BBRefID[0]
 
-#TODO for names with 3 names
-# try:
-#     if num_names(player_name) == 3:
-#         pname0,pname1,pname2 = player_name.split(" ")
-#         PlayerFirstName = pname0 + " " + pname1
-#         PlayerLastName = pname2
-#     else:
-#         PlayerFirstName,PlayerLastName=player_name.split(" ")
-#     Player_ID_Dataframe = playerid_lookup(PlayerLastName,PlayerFirstName)
-#     print(Player_ID_Dataframe)
-
-# except ValueError as e:
-#     print(f'Problem searching for {player_name}\nerror: {e}')
-
-SanitizedIDFrame = Player_ID_Dataframe.loc[Player_ID_Dataframe['mlb_played_last']==2021]
-BBRefKey = SanitizedIDFrame['key_bbref']
-
-SanitizedBBRefKey = BBRefKey.to_string(index=False)
-BBRefLetter = SanitizedBBRefKey[0]
-
-hyperlink_Main_Page = "\fhttps://www.baseball-reference.com/players/%s/%s.shtml" % (BBRefLetter,SanitizedBBRefKey)
+hyperlink_Main_Page = "\fhttps://www.baseball-reference.com/players/%s/%s.shtml" % (BBRefLetter,BBRefID)
 print(hyperlink_Main_Page)
 
-hyperlink_GameLogs = "\fhttps://www.baseball-reference.com/players/gl.fcgi?id=%s&t=%s&year=%d" % (SanitizedBBRefKey,Player_Type,Year)
+hyperlink_GameLogs = "\fhttps://www.baseball-reference.com/players/gl.fcgi?id=%s&t=%s&year=%d" % (BBRefID,b_or_p,Year)
 print(hyperlink_GameLogs)
 
-hyperlink_Splits = "\fhttps://www.baseball-reference.com/players/split.fcgi?id=%s&year=%d&t=%s" % (SanitizedBBRefKey,Year,Player_Type)
+hyperlink_Splits = "\fhttps://www.baseball-reference.com/players/split.fcgi?id=%s&year=%d&t=%s" % (BBRefID,Year,b_or_p)
 print(hyperlink_Splits)
 
-hyperlink_AdvancedStats = "\fhttps://www.baseball-reference.com/players/%s/%s-%s.shtml" % (BBRefLetter,SanitizedBBRefKey,Bat_Or_Pitch)
+hyperlink_AdvancedStats = "\fhttps://www.baseball-reference.com/players/%s/%s-%s.shtml" % (BBRefLetter,BBRefID,bat_or_pitch)
 print (hyperlink_AdvancedStats)
