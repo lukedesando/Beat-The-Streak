8fe2fe2eac3d253423aacb68dbec3dd12ae4fb5c
lukedesando <Lukedesando@gmail.com>
2021-08-26 13:48:23 -0400

    Added New URLs

--- BBRef Scrape.py
+++ BBRef Scrape.py
@@ -25,14 +25,22 @@ else:
 #NOTE This will give the table for gamelogs of a specified year off BBRef
 
 #url = '''\fhttps://widgets.sports-reference.com/wg.fcgi?css=1&site=br&url=%2Fplayers%2Fgl.fcgi%3Fid%3D{PlayerID}%26t%3Dp%26year%3D{year}&div=div_{batting_or_pitching}_gamelogs'''
-url = '''https://widgets.sports-reference.com/wg.fcgi?css=1&site=br&url=%2Fplayers%2Fgl.fcgi%3F\
+GameLogsURL = '''https://widgets.sports-reference.com/wg.fcgi?css=1&site=br&url=%2Fplayers%2Fgl.fcgi%3F\
 id%3D{}%26t%3D{}%26year%3D{}&div=div_{}_gamelogs'''.format(PlayerID,b_or_p,year,batting_or_pitching)
-#four different fields to change
+
+SplitsSeasonTotalsURL = '''https://widgets.sports-reference.com/wg.fcgi?css=1&site=br&url=%2Fplayers%2Fsplit.fcgi%3F\
+id%3D{}%26year%3D{}%26t%3D{}&div=div_total'''.format(PlayerID,year,b_or_p)
 
 ## remove the junk out of the dataframe
-GameLogs=pd.read_html(url)[0].query('aLI != "aLI"')\
+GameLogs=pd.read_html(GameLogsURL)[0].query('aLI != "aLI"')\
     .drop('Unnamed: 5', axis=1)\
     .apply(partial(pd.to_numeric, errors='ignore'))\
     .reset_index(drop=True)
 
-GameLogs.to_csv(PlayerName + " Game Logs.csv",index=False)
+Splits = pd.read_html(SplitsSeasonTotalsURL)[0].query('G != "G"')\
+    .apply(partial(pd.to_numeric, errors='ignore'))\
+    .reset_index(drop=True)
+
+# GameLogs.to_csv(PlayerName + " Game Logs.csv",index=False)
+
+print(Splits)
