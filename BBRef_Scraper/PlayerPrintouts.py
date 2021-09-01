6d2095adbd1965a6462ff5b88abcb72ba347c833
lukedesando <Lukedesando@gmail.com>
2021-09-01 19:40:33 -0400

    Renamed File, added Printouts by ID

--- BBRef_Scraper/PlayerPrintouts.py
+++ BBRef_Scraper/PlayerPrintouts.py
@@ -53,6 +53,48 @@ BatterStats=['hits','avg','babip','strikeOuts','baseOnBalls','obp','ops']):
         print (f"{stat}: {BatterStatsDict['stats'][0]['stats'][stat]}")
     print()
 
+def GetPitcherPrintoutMLBID(PitcherName,PitcherBasics=['current_team','pitch_hand'],
+PitcherStats = ['gamesStarted', 'strikeOuts', 'era','avg','whip','hits','hitsPer9Inn']):
+    '''Pitcher Basics include First Name, Last Name, Current Team, and Pitching Hand by default
+    \nPitcher Stats are GS, SO, ERA, AVG, WHIP, Hits, and H/9 by default'''
+    
+    PitcherID = Get_MLB_ID(PitcherName)
+    try:
+        PitcherStatsDict = player_stat_data(PitcherID,group='pitching')
+        print(PitcherName)
+        
+        #PitcherBasics = ['first_name','last_name','pitch_hand']
+        for stat in PitcherBasics:
+            print (PitcherStatsDict.get(stat))
+
+        print(PitcherID)
+
+        #PitcherStats = ['gamesStarted', 'strikeOuts', 'era','avg','whip','hits','hitsPer9Inn',]
+        for stat in PitcherStats:
+            print (f"{stat}: {PitcherStatsDict['stats'][0]['stats'][stat]}")
+    except:
+        print("Can't print due to error")
+    #NOTE: needed to separate the pitchers
+    print()
+
+def GetBatterPrintoutMLBID(BatterID,BatterBasics=['first_name','last_name','current_team','bat_side'],
+BatterStats=['hits','avg','babip','strikeOuts','baseOnBalls','obp','ops']):
+    '''Batter Basics include First Name, Last Name, Current Team, and Batting Side by default
+    \nBatter Stats are H, AVG, BABIP, SO, BB, OBP, and OPS by default'''
+    
+    #BatterID = Get_MLB_ID(BatterName)
+    BatterStatsDict = player_stat_data(BatterID,group='batting')
+    #print(BatterName)
+    
+    for stat in BatterBasics:
+        print (BatterStatsDict.get(stat))
+
+    print(BatterID)
+
+    for stat in BatterStats:
+        print (f"{stat}: {BatterStatsDict['stats'][0]['stats'][stat]}")
+    print()
+
 def StartingPitchersPrintout(StartDate=Today,EndDate=Today):
     '''Today is selected by default as the start and end dates
     \n Encounters errors if used with past dates'''
@@ -68,7 +110,7 @@ def StartingPitchersTest(StartDate=Today,EndDate=Today):
     print (StartingPitchersdf)
     print ()
 
-StartingPitchersPrintout()
+#StartingPitchersPrintout()
 
 # starters = espn.ProbableStartersScraper(Today, Today).scrape()
 # StartingPitchersdf = starters['Name']
