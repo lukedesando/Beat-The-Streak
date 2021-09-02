5688a2dc41ebcd79c62e42629757c40268292ebd
lukedesando <Lukedesando@gmail.com>
2021-09-02 02:08:58 -0400

    Exception grant for Shohei Ohtani

--- StatcastScrape.py
+++ StatcastScrape.py
@@ -15,25 +15,23 @@ YesterdayString = str(Yesterday)
 FiveDaysAgoString = str(FiveDaysAgo)
 BeginningofYearString = '2021-01-01'
 
-def statcast_player(PlayerName=None,start_dt=FiveDaysAgoString, end_dt=TodayString, MLBID=None):
+def statcast_player(PlayerName=None,start_dt=FiveDaysAgoString, end_dt=TodayString, MLBID=None, position=None):
     "Works by specifying either player name or MLBID"
-    if MLBID != None:
-        position = CheckPosition(MLBID)
-    if PlayerName != None:
-        position = CheckPosition(PlayerName)
+    if MLBID == None:
         MLBID = Get_MLB_ID(PlayerName)
+    if position == None:
+        position = CheckPosition(MLBID)
     if position == 'P':
         return statcast_pitcher(start_dt,end_dt,MLBID)
     else:
         return statcast_batter(start_dt,end_dt,MLBID)
 
-def print_statcast_player(PlayerName=None, start_dt=FiveDaysAgoString, end_dt=TodayString, MLBID=None):
+def print_statcast_player(PlayerName=None, start_dt=FiveDaysAgoString, end_dt=TodayString, MLBID=None, position=None):
     "Works by specifying either player name or MLBID"
-    if MLBID != None:
-        position = CheckPosition(MLBID)
-    if PlayerName != None:
-        position = CheckPosition(PlayerName)
+    if MLBID == None:
         MLBID = Get_MLB_ID(PlayerName)
+    if position == None:
+        position = CheckPosition(MLBID)
     print(MLBID)
     if position == 'P':
         print(statcast_pitcher(start_dt,end_dt,MLBID))
