c270ea62950ac97d0f52ff5cfb8254bbba8188da
lukedesando <Lukedesando@gmail.com>
2021-09-02 01:19:28 -0400

    CheckPosition, merged multiple functions into one with if

--- StatcastScrape.py
+++ StatcastScrape.py
@@ -1,52 +1,41 @@
 ## imports
-from BackgroundFunctions import Get_BBRef_ID, Get_MLB_ID, Get_BBRefID_From_MLBID, Check_bat_or_pitch
+from BackgroundFunctions import CheckPosition, Get_BBRef_ID, Get_MLB_ID, Get_BBRefID_From_MLBID, Check_bat_or_pitch
 from baseball_scraper import statcast_batter, statcast_pitcher
 from datetime import date, timedelta
 
 Today = date.today()
 Tomorrow = date.today() + timedelta(days=1)
 Yesterday = date.today() + timedelta(days=-1)
+FiveDaysAgo = date.today() + timedelta(days=-5)
 CurrentYear = date.today().year
 
 TodayString = str(Today)
 TomorrowString = str(Tomorrow)
 YesterdayString = str(Yesterday)
+FiveDaysAgoString = str(FiveDaysAgo)
 BeginningofYearString = '2021-01-01'
 
-def statcast_player_MLBID(start_dt=None, end_dt=TodayString, MLBID=None):
-    BBRefID=Get_BBRefID_From_MLBID(MLBID)
-    bat_or_pitch = Check_bat_or_pitch(BBRefID)
-    if bat_or_pitch == 'bat':
-        return statcast_batter(start_dt,end_dt,MLBID)
-    else:
+def statcast_player(PlayerName=None,start_dt=FiveDaysAgoString, end_dt=TodayString, MLBID=None):
+    "Works by specifying either player name or MLBID"
+    if MLBID != None:
+        position = CheckPosition(MLBID)
+    if PlayerName != None:
+        position = CheckPosition(PlayerName)
+        MLBID = Get_MLB_ID(PlayerName)
+    if position == 'P':
         return statcast_pitcher(start_dt,end_dt,MLBID)
-
-def statcast_player(start_dt=None, end_dt=TodayString, PlayerName=None):
-    MLBID = Get_MLB_ID(PlayerName)
-    BBRefID=Get_BBRefID_From_MLBID(MLBID)
-    bat_or_pitch = Check_bat_or_pitch(BBRefID)
-    if bat_or_pitch == 'bat':
+    else:
         return statcast_batter(start_dt,end_dt,MLBID)
-    else:
-        return statcast_pitcher(start_dt,end_dt,MLBID)
 
-def print_statcast_player_MLBID(start_dt=None, end_dt=TodayString, MLBID=None):
-    'Needs MLBID'
-    BBRefID=Get_BBRefID_From_MLBID(MLBID)
-    bat_or_pitch = Check_bat_or_pitch(BBRefID)
-    if bat_or_pitch == 'bat':
-        print(statcast_batter(start_dt,end_dt,MLBID))
-    else:
+def statcast_player_print(PlayerName=None, start_dt=FiveDaysAgoString, end_dt=TodayString, MLBID=None):
+    "Works by specifying either player name or MLBID"
+    if MLBID != None:
+        position = CheckPosition(MLBID)
+    if PlayerName != None:
+        position = CheckPosition(PlayerName)
+        MLBID = Get_MLB_ID(PlayerName)
+    print(MLBID)
+    if position == 'P':
         print(statcast_pitcher(start_dt,end_dt,MLBID))
-
-def print_statcast_player(start_dt=None, end_dt=TodayString, PlayerName=None):
-    'Needs MLBID'
-    MLBID = Get_MLB_ID(PlayerName)
-    BBRefID=Get_BBRefID_From_MLBID(MLBID)
-    bat_or_pitch = Check_bat_or_pitch(BBRefID)
-    if bat_or_pitch == 'bat':
-        print(statcast_batter(start_dt,end_dt,MLBID))
     else:
-        print(statcast_pitcher(start_dt,end_dt,MLBID))
-
-#print(statcast_player(start_dt = BeginningofYearString,end_dt=TodayString,PlayerName='Juan Soto'))
+        print(statcast_batter(start_dt,end_dt,MLBID))
