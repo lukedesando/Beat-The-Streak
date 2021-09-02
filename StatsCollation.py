daa885b00b3b44e570e7a20b9b64e3f3f0ecea8f
lukedesando <Lukedesando@gmail.com>
2021-09-02 01:53:33 -0400

    More Testing

--- StatsCollation.py
+++ StatsCollation.py
@@ -4,9 +4,9 @@
 # totals, last 7 and 14 days
 
 ## imports
-from BackgroundFunctions import Get_BBRef_ID, Get_MLB_ID, Get_BBRefID_From_MLBID
-from BBRefScrape import PrintAllSplits, PrintAllSplitsBBRefID
-from PlayerPrintouts import PrintPlayerStats, PrintPlayerStatsMLBID
+from BackgroundFunctions import CheckPosition, Get_BBRef_ID, Get_MLB_ID, Get_BBRefID_From_MLBID
+from BBRefScrape import PrintAllSplits
+from PlayerPrintouts import PrintPlayerStats
 from StatcastScrape import statcast_player, print_statcast_player
 from datetime import date, timedelta
 
@@ -23,7 +23,7 @@ BeginningofYearString = '2021-01-01'
 ThirtyDaysAgoString = str(ThirtyDaysAgo)
 
 
-PlayerList = ['Gerrit Cole', "Juan Soto"]
+PlayerList = ['Casey Mize', "Juan Soto",'Robbie Ray']
 
 #print(Check_bat_or_pitch('scherma01'))
 
@@ -39,8 +39,20 @@ PlayerList = ['Gerrit Cole', "Juan Soto"]
 #     BBRefID = Get_BBRef_ID(player)
 #     PrintPlayerStatsMLBID(MLBID)
 #     PrintAllSplitsBBRefID(BBRefID)
+for players in PlayerList:
+    PrintPlayerStats(players)
+    print_statcast_player(players)
+    PrintAllSplits(players)
+# #NOTE: Just for printing!
+# for player in PlayerList:
+#     #PrintPlayerStats(player)
+#     PrintAllSplits(player)
+#     #print_statcast_player(start_dt= ThirtyDaysAgoString,PlayerName=player)
 
-for player in PlayerList:
-    PrintPlayerStats(player)
-    PrintAllSplits(player)
-    print_statcast_player(start_dt= ThirtyDaysAgoString,PlayerName=player)
+
+# for player in PlayerList:
+#     StatcastDF = statcast_player(start_dt=ThirtyDaysAgoString,PlayerName = player)
+#     StatcastDF.to_csv('Statcast Data '+player+'.csv',index=False)
+
+#PrintAllSplits(BBRefID='sotoju01')
+#MyStatsdf = MyPitcherStats('Gerrit Cole')
