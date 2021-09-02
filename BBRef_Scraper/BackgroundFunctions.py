0f343d1e13c2582356ba91c097e0187c30ec6f00
lukedesando <Lukedesando@gmail.com>
2021-09-01 21:07:34 -0400

    Check_bat_or_pitch, get_BBRef from MLB

--- BBRef_Scraper/BackgroundFunctions.py
+++ BBRef_Scraper/BackgroundFunctions.py
@@ -1,7 +1,7 @@
 import csv
 from numpy import True_
 from statsapi import player_stats,player_stat_data,lookup_player,schedule
-from baseball_scraper import playerid_lookup
+from baseball_scraper import playerid_lookup,playerid_reverse_lookup
 import pandas as pd
 from datetime import date, datetime,timedelta
 
@@ -63,6 +63,16 @@ def Get_BBRef_ID(PlayerName):
     SanitizedBBRefKey = BBRefKey.to_string(index=False)
     return SanitizedBBRefKey
 
+def Get_BBRefID_From_MLBID(MLBID):
+    "Needs to import MLBID as a list"
+    if not isinstance(MLBID,list):
+        MLBID = [MLBID]
+    Player_ID_Dataframe = playerid_reverse_lookup(MLBID)
+    SanitizedIDFrame = Player_ID_Dataframe.loc[Player_ID_Dataframe['mlb_played_last']==2021]
+    BBRefKey = SanitizedIDFrame['key_bbref']
+    SanitizedBBRefKey = BBRefKey.to_string(index=False)
+    return SanitizedBBRefKey
+
 #print(Get_BBRef_ID("Chi Chi Gonzalez"))
 
 #NOTE Relies on external database; will need to be updated occasionally
@@ -78,12 +88,28 @@ def Find_Fangraph_ID(player_name):
             return int(FGid)
 
 #FIXME: Need an exception for Shohei Ohtani
-def Check_batting_or_pitching(PlayerID,year=CurrentYear):
-    "Widget exists only for pitchers. If it exists, player is pitcher. If not, player is hitter"
+def Check_batting_or_pitching(BBRefPlayerID,year=CurrentYear):
+    '''Widget exists only for pitchers. If it exists, player is pitcher. If not, player is hitter\n
+    Returns 3 values'''
     try:
         SplitsSeasonTotalsPitchersURL = '''https://widgets.sports-reference.com/wg.fcgi?css=1&site=br&url=%2Fplayers%2Fsplit.fcgi%3F\
-id%3D{}%26year%3D{}%26t%3Dp&div=div_total_extra'''.format(PlayerID,year)
+id%3D{}%26year%3D{}%26t%3Dp&div=div_total_extra'''.format(BBRefPlayerID,year)
         pd.read_html(SplitsSeasonTotalsPitchersURL)
         return "pitching","pitch","p"
     except ImportError:
         return "batting","bat","b"
+
+def Check_bat_or_pitch(BBRefPlayerID,year=CurrentYear):
+    "Returns 1 value"
+    try:
+        SplitsSeasonTotalsPitchersURL = '''https://widgets.sports-reference.com/wg.fcgi?css=1&site=br&url=%2Fplayers%2Fsplit.fcgi%3F\
+id%3D{}%26year%3D{}%26t%3Dp&div=div_total_extra'''.format(BBRefPlayerID,year)
+        pd.read_html(SplitsSeasonTotalsPitchersURL)
+        return "pitch"
+    except ImportError:
+        return "bat"
+
+def MakeList(MyString):
+    if not isinstance(MyString,list):
+        MyString = [MyString]
+    return MyString
