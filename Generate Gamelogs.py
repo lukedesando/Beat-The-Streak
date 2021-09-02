2a8d0ad4c301d3fb343b55ab4e07b40ad9464787
lukedesando <Lukedesando@gmail.com>
2021-09-02 02:42:12 -0400

    Dynamic gamelogs from MLBID or player name

--- Generate Gamelogs.py
+++ Generate Gamelogs.py
@@ -4,26 +4,67 @@ import pandas as pd
 from functools import partial
 from datetime import date
 
-
+PlayerList = ['Casey Mize', "Juan Soto",'Robbie Ray']
 CurrentYear = date.today().year
 PlayerName = "Juan Soto"
 #batting_or_pitching = "pitching"
 year = 2021
 
+#PlayerID = "corbipa01"
 
 #XXX Reduce testing time by a few seconds by not going through the get ID function
-PlayerID = Get_BBRef_ID(PlayerName)
-#PlayerID = "corbipa01"
-batting_or_pitching, bat_or_pitch,b_or_p = Check_batting_or_pitching(PlayerID)
+def GenerateGamelogCSV(PlayerName = None,BBRefID=None):
+    if BBRefID == None:
+        BBRefID = Get_BBRef_ID(PlayerName)
 
-GameLogsURL = '''https://widgets.sports-reference.com/wg.fcgi?css=1&site=br&url=%2Fplayers%2Fgl.fcgi%3F\
-id%3D{}%26t%3D{}%26year%3D{}&div=div_{}_gamelogs'''.format(PlayerID,b_or_p,year,batting_or_pitching)
+    batting_or_pitching, bat_or_pitch,b_or_p = Check_batting_or_pitching(BBRefID)
 
-## removes the junk out of a given dataframe
+    GameLogsURL = '''https://widgets.sports-reference.com/wg.fcgi?css=1&site=br&url=%2Fplayers%2Fgl.fcgi%3F\
+id%3D{}%26t%3D{}%26year%3D{}&div=div_{}_gamelogs'''.format(BBRefID,b_or_p,year,batting_or_pitching)
 
-PlayerGameLogs=pd.read_html(GameLogsURL)[0].query('R != "R"')\
-    .drop('Unnamed: 5', axis=1)\
-    .apply(partial(pd.to_numeric, errors='ignore'))\
-    .reset_index(drop=True)
+    ## removes the junk out of the gamelogs
+    PlayerGameLogs=pd.read_html(GameLogsURL)[0].query('R != "R"')\
+        .drop('Unnamed: 5', axis=1)\
+        .apply(partial(pd.to_numeric, errors='ignore'))\
+        .reset_index(drop=True)
 
-PlayerGameLogs.to_csv(PlayerName + " Game Logs.csv",index=False)
+    PlayerGameLogs.to_csv("GameLogs "+ PlayerName + ".csv",index=False)
+
+def GenerateGamelog(PlayerName = None,BBRefID=None):
+    if BBRefID == None:
+        BBRefID = Get_BBRef_ID(PlayerName)
+
+    batting_or_pitching, bat_or_pitch,b_or_p = Check_batting_or_pitching(BBRefID)
+
+    GameLogsURL = '''https://widgets.sports-reference.com/wg.fcgi?css=1&site=br&url=%2Fplayers%2Fgl.fcgi%3F\
+id%3D{}%26t%3D{}%26year%3D{}&div=div_{}_gamelogs'''.format(BBRefID,b_or_p,year,batting_or_pitching)
+
+    ## removes the junk out of the gamelogs
+    PlayerGameLogs=pd.read_html(GameLogsURL)[0].query('R != "R"')\
+        .drop('Unnamed: 5', axis=1)\
+        .apply(partial(pd.to_numeric, errors='ignore'))\
+        .reset_index(drop=True)
+
+    return PlayerGameLogs
+
+def PrintGamelog(PlayerName = None,BBRefID=None):
+    if BBRefID == None:
+        BBRefID = Get_BBRef_ID(PlayerName)
+
+    batting_or_pitching, bat_or_pitch,b_or_p = Check_batting_or_pitching(BBRefID)
+
+    GameLogsURL = '''https://widgets.sports-reference.com/wg.fcgi?css=1&site=br&url=%2Fplayers%2Fgl.fcgi%3F\
+id%3D{}%26t%3D{}%26year%3D{}&div=div_{}_gamelogs'''.format(BBRefID,b_or_p,year,batting_or_pitching)
+
+    ## removes the junk out of the gamelogs
+    PlayerGameLogs=pd.read_html(GameLogsURL)[0].query('R != "R"')\
+        .drop('Unnamed: 5', axis=1)\
+        .apply(partial(pd.to_numeric, errors='ignore'))\
+        .reset_index(drop=True)
+
+    print(PlayerGameLogs)
+
+for player in PlayerList:
+    GenerateGamelogCSV(player)
+    PrintGamelog(player)
+    print(GenerateGamelog(player))
