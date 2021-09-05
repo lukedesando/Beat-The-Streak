70e3cb57f1f7ec3fa5ccc84425189514d7ffc813
lukedesando <Lukedesando@gmail.com>
2021-09-05 00:47:50 -0400

    Turned three functions into one

--- PlayerPrintouts.py
+++ PlayerPrintouts.py
@@ -1,4 +1,3 @@
-from bs4.element import TemplateString
 from statsapi import player_stats,player_stat_data,lookup_player
 import pandas as pd
 from BackgroundFunctions import CheckPosition, Get_MLB_ID, rosterPlayers,ESPNTeamIDtoMLBTeamID
@@ -9,58 +8,6 @@ import statsapi
 Today = datetime.today()
 Tomorrow = datetime.today() + timedelta(days=1)
 
-def PrintPitcherStats(PlayerName=None,MLBID=None,PitcherBasics=['first_name','last_name','current_team','position','pitch_hand'],
-PitcherStats = ['gamesStarted', 'strikeOuts', 'era','avg','whip','hits','hitsPer9Inn']):
-    '''Pitcher Basics include First Name, Last Name, Current Team, and Pitching Hand by default
-    \nPitcher Stats are GS, SO, ERA, AVG, WHIP, Hits, and H/9 by default'''
-    
-    if MLBID == None:
-        MLBID = Get_MLB_ID(PlayerName)
-        
-    try:
-        PitcherStatsDict = player_stat_data(MLBID,group='pitching')
-        
-        #PitcherBasics = ['first_name','last_name','pitch_hand']
-        for stat in PitcherBasics:
-            print (PitcherStatsDict.get(stat))
-
-        print(MLBID)
-
-        #PitcherStats = ['gamesStarted', 'strikeOuts', 'era','avg','whip','hits','hitsPer9Inn',]
-        for stat in PitcherStats:
-            print (f"{stat}: {PitcherStatsDict['stats'][0]['stats'][stat]}")
-    except:
-        print("Can't print due to error")
-    #NOTE: needed to separate the pitchers
-    print()
-
-def PrintBatterStats(PlayerName=None,MLBID=None,BatterBasics=['first_name','last_name','current_team','position','bat_side'],
-BatterStats=['hits','avg','babip','strikeOuts','baseOnBalls','obp','ops']):
-    '''Batter Basics include First Name, Last Name, Current Team, and Batting Side by default
-    \nBatter Stats are H, AVG, BABIP, SO, BB, OBP, and OPS by default'''
-    
-    if MLBID == None:
-        MLBID = Get_MLB_ID(PlayerName)
-
-    BatterStatsDict = player_stat_data(MLBID,group='batting')
-    
-    for stat in BatterBasics:
-        print (BatterStatsDict.get(stat))
-
-    print(MLBID)
-
-    for stat in BatterStats:
-        print (f"{stat}: {BatterStatsDict['stats'][0]['stats'][stat]}")
-    print()
-
-def StartingPitchersPrintout(StartDate=Today,EndDate=Today):
-    '''Today is selected by default as the start and end dates
-    \n Encounters errors if used with past dates'''
-    starters = espn.ProbableStartersScraper(StartDate, EndDate).scrape()
-    StartingPitchersdf = starters['Name']
-    for pitcher in StartingPitchersdf:
-        PrintPitcherStats(pitcher)
-
 #NOTE: Only needed to test if Starting pitchrs
 def StartingPitchersTest(StartDate=Today,EndDate=Today):
     starters = espn.ProbableStartersScraper(StartDate, EndDate).scrape()
@@ -68,16 +15,36 @@ def StartingPitchersTest(StartDate=Today,EndDate=Today):
     print (StartingPitchersdf)
     print ()
 
-def PrintPlayerStats(PlayerName=None,MLBID=None):
+def PrintPlayerStats(PlayerName=None,MLBID=None, position = None,
+BatterBasics=['first_name','last_name','current_team','position','bat_side'],
+BatterStats=['hits','avg','babip','strikeOuts','baseOnBalls','obp','ops'],
+PitcherBasics=['first_name','last_name','current_team','position','pitch_hand'],
+PitcherStats = ['gamesStarted', 'strikeOuts', 'era','avg','whip','hits','hitsPer9Inn','walksPer9Inn']):
+
     if MLBID == None:
         if PlayerName != None:
             MLBID = Get_MLB_ID(PlayerName)
+    if position == None:
+        position = CheckPosition(MLBID)
+    
 
-    position = CheckPosition(MLBID)
     if position == 'P':
-        PrintPitcherStats(MLBID=MLBID)
+        PlayerStatsDict = player_stat_data(MLBID,group='pitching')
+        PlayerBasics = PitcherBasics
+        PlayerStats = PitcherStats
     else:
-        PrintBatterStats(MLBID=MLBID)
+        PlayerStatsDict = player_stat_data(MLBID,group='batting')
+        PlayerBasics = BatterBasics
+        PlayerStats = BatterStats
+
+    for stat in PlayerBasics:
+        print (PlayerStatsDict.get(stat))
+
+    print(MLBID)
+
+    for stat in PlayerStats:
+        print (f"{stat}: {PlayerStatsDict['stats'][0]['stats'][stat]}")
+    print()
 
 def StartingPitchersPrintoutwRoster(StartDate=Today,EndDate=Today):
     '''Today is selected by default as the start and end dates
@@ -88,12 +55,31 @@ def StartingPitchersPrintoutwRoster(StartDate=Today,EndDate=Today):
     for i, row in starters.iterrows():
         pitcher = row['Name']
         opponent = row['opponent']
-        PrintPitcherStats(pitcher)
-        print(rosterPlayers(ESPNTeamIDtoMLBTeamID(opponent)))
-        print()
+        PrintPlayerStats(pitcher)
+        print("Opponents: ")
+        RosterBatterStatsPrintout(ESPNTeamIDtoMLBTeamID(opponent))
+        print("Next Pitcher :")
         #print(starters['Name'])
         #PrintPitcherStats(pitcher)
         #print(starters['opponent'][pitcher])
+
+def RosterBatterStatsPrintout(MLBTeamID):
+    Roster = rosterPlayers(MLBTeamID)
+    for player in Roster:
+        PlayerID = player[1]
+        PlayerPosition = player[2]
+        if PlayerPosition != 'P':
+            PrintPlayerStats(MLBID = PlayerID, position = PlayerPosition)
+
+
+#StartingPitchersPrintoutwRoster()
+
+PlayerList = ["Max Scherzer","Kris Bryant","Javier Baez","Jose Berrios","Craig Kimbrel"]
+
+for Player in PlayerList:
+    PrintPlayerStats(Player)
+
+#RosterBatterStatsPrintout(120)
 
 # starters = espn.ProbableStartersScraper(Tomorrow, Tomorrow).scrape()
 # for i, row in starters.iterrows():
