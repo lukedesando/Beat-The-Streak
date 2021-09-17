f3595d613ce93b261df519d5ae29aa7d74073843
lukedesando <Lukedesando@gmail.com>
2021-09-17 14:07:50 -0400

    Daily Play (Precursor to Init)

---
+++ Daily Play.py
@@ -0,0 +1,20 @@
+from StatcastScrape import BeginningofYearString, print_statcast_player, statcast_player
+from BackgroundFunctions import Tomorrow
+from BBRefScrape import GetAllSplits
+from PlayerPrintouts import PrintPlayerStats, StartingPitchersPrintout
+
+
+#FIXME: Pitcher with same name as hitter is mistaken for hitter
+#StartingPitchersPrintout(Tomorrow,Tomorrow)
+
+#TODO Update Chadwick Now and again
+
+PlayerList = ["Miguel Cabrera"]#, "Jeimer Candelario","Harold Castro","Jonathan Schoop"]
+
+for Player in PlayerList:
+    print(Player)
+    GetAllSplits(Player)
+    statcast_player(Player,BeginningofYearString).to_csv("Statcast " + Player + ".csv")
+    print_statcast_player(Player,BeginningofYearString)
+    #TODO: Batter vs. Pitcher stats
+    #TODO: Batter vs. Team stats
