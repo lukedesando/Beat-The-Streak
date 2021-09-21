941375f9474595ff726433fa9e7f4e7102f1b203
lukedesando <Lukedesando@gmail.com>
2021-09-21 15:27:52 -0400

    Added Events arg

--- StatcastScrape.py
+++ StatcastScrape.py
@@ -1,19 +1,24 @@
 ## imports
+from pandas.core.frame import DataFrame
 from BackgroundFunctions import CheckPosition, Get_BBRef_and_MLB_ID, Get_MLB_ID, Get_BBRefID_From_MLBID, Check_bat_or_pitch
 from baseball_scraper import statcast_batter, statcast_pitcher
 from datetime import date, timedelta
-from BackgroundFunctions import FiveDaysAgoString,TodayString
+from BackgroundFunctions import FiveDaysAgoString,TodayString, BeginningofYearString
 
-def statcast_player(PlayerName=None,start_string=FiveDaysAgoString, end_string=TodayString, MLBID=None, position=None):
+def statcast_player(PlayerName=None,start_string=BeginningofYearString, end_string=TodayString, MLBID=None, position=None, Events=False):
     "Works by specifying either player name or MLBID"
+    StatcastDataframe = DataFrame()
     if MLBID == None:
         MLBID = Get_MLB_ID(PlayerName)
     if position == None:
         position = CheckPosition(MLBID)
     if position == 'P':
-        return statcast_pitcher(start_string,end_string,MLBID)
+        StatcastDataframe = statcast_pitcher(start_string,end_string,MLBID)
     else:
-        return statcast_batter(start_string,end_string,MLBID)
+        StatcastDataframe = statcast_batter(start_string,end_string,MLBID)
+    if Events == True:
+        StatcastDataframe = StatcastDataframe.dropna(subset=['events'])
+    return StatcastDataframe
 
 def print_statcast_player(PlayerName=None, start_string=FiveDaysAgoString, end_string=TodayString, MLBID=None, position=None):
     "Works by specifying either player name or MLBID"
@@ -27,5 +32,4 @@ def statcast_player_csv(PlayerName=None,start_string=FiveDaysAgoString, end_stri
     CSVtoPrint.to_csv("Statcast " + str(MLBID) + " " + PlayerName +".csv")
 # Testing Functions
 if __name__ == '__main__':
-    statcast_player_csv("Buster Posey",BeginningofYearString)
-    statcast_player_csv("Clayton Kershaw",BeginningofYearString)
+    print(statcast_player("Buster Posey", Events=True))
