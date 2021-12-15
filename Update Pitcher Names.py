3007af2d86459b5ec696f793eb05c481c0b323d2
lukedesando <Lukedesando@gmail.com>
2021-12-15 14:09:45 -0500

    Updates through Chadwick now

--- Update Pitcher Names.py
+++ Update Pitcher Names.py
@@ -3,11 +3,11 @@ import sys
 from getpass import getpass
 import mariadb
 #FIXME: Need to import from Beat-The-Streak repo
-from BackgroundFunctions import Get_Player_Name, Get_Player_Name_Last_First
+from BackgroundFunctions import Get_Player_Name, Get_Player_Name_Last_First, Chadwick_Get_Name_Last_First
 
 localhost = '192.168.1.152'
 
-select_pitcherID_query = "SELECT pitcher, pitcher_name FROM CombinedEvents WHERE pitcher_name IS Null"
+select_pitcherID_query = "SELECT pitcher, pitcher_name FROM CombinedEvents WHERE pitcher_name IS Null group by pitcher"
 update_pitcherName_query = '''UPDATE CombinedEvents SET pitcher_name = ? WHERE pitcher = ?'''
 CurrentDB = "GameLogs"
 
@@ -26,7 +26,8 @@ try:
     emptynames = cur.fetchall()
     for PitcherID, PitcherName in emptynames:
         try:
-            PitcherName = Get_Player_Name_Last_First(PitcherID)
+            # PitcherName = Get_Player_Name_Last_First(PitcherID) - faster, but incomplete for former players
+            PitcherName = Chadwick_Get_Name_Last_First(PitcherID)
             cur2.execute (update_pitcherName_query,(PitcherName, PitcherID))
             conn.commit()
             # cur2.execute (f'''UPDATE CombinedEvents SET pitcher_name = {PitcherName} WHERE pitcher = {PitcherID}''')
