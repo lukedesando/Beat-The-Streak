b4d7a8762ca59adcb2c8031844835589512fc696
lukedesando <Lukedesando@gmail.com>
2021-12-15 12:02:38 -0500

    Function Works Now

--- Update Pitcher Names.py
+++ Update Pitcher Names.py
@@ -1,14 +1,14 @@
-from mariadb import connect, Error
+from mariadb import connect, Error, Warning
 import sys
 from getpass import getpass
 import mariadb
 #FIXME: Need to import from Beat-The-Streak repo
-from BackgroundFunctions import Get_Player_Name
+from BackgroundFunctions import Get_Player_Name, Get_Player_Name_Last_First
 
 localhost = '192.168.1.152'
 
 select_pitcherID_query = "SELECT pitcher, pitcher_name FROM CombinedEvents WHERE pitcher_name IS Null"
-update_pitcherName_query = "UPDATE CombinedEvents"
+update_pitcherName_query = '''UPDATE CombinedEvents SET pitcher_name = ? WHERE pitcher = ?'''
 CurrentDB = "GameLogs"
 
 
@@ -21,10 +21,19 @@ try:
         # password=getpass("Enter password: "),
     )
     cur = conn.cursor()
+    cur2 = conn.cursor()
     cur.execute(select_pitcherID_query)
-    for PitcherID, PitcherName in cur:
-
-        print(f"PitcherID: {PitcherID}, PitcherName: {PitcherName}")
+    emptynames = cur.fetchall()
+    for PitcherID, PitcherName in emptynames:
+        try:
+            PitcherName = Get_Player_Name_Last_First(PitcherID)
+            cur2.execute (update_pitcherName_query,(PitcherName, PitcherID))
+            conn.commit()
+            # cur2.execute (f'''UPDATE CombinedEvents SET pitcher_name = {PitcherName} WHERE pitcher = {PitcherID}''')
+            print(f"PitcherID: {PitcherID}, PitcherName: {PitcherName}")
+        except (Error, Warning) as e:
+            print(f"PitcherID: {PitcherID}, PitcherName: {PitcherName}")
+            print("Didn't Work")
 except Error as e:
     print(e)
     sys.exit(1)
