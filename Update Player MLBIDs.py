3ea917ee688f7226a72d99a263c4e1226dd4099f
lukedesando <Lukedesando@gmail.com>
2021-12-16 02:39:14 -0500

    Updated HardHitLeaders

---
+++ Update Player MLBIDs.py
@@ -0,0 +1,42 @@
+from contextlib import nullcontext
+from mariadb import connect, Error, Warning
+import sys
+from getpass import getpass
+import mariadb
+from numpy import empty
+#FIXME: Need to import from Beat-The-Streak repo
+from BackgroundFunctions import Get_MLB_ID
+
+localhost = '192.168.1.152'
+
+select_Player_query = "SELECT MLBID, Player FROM HardHitLeaders"
+update_MLBID_query = '''UPDATE HardHitLeaders SET MLBID = ? WHERE Player = ?'''
+CurrentDB = "GameLogs"
+
+
+try:
+    conn = mariadb.connect(
+        host='192.168.1.152',
+        user='Luke',
+        # user=input("Enter username: "),
+        database = CurrentDB
+        # password=getpass("Enter password: "),
+    )
+    cur = conn.cursor()
+    cur2 = conn.cursor()
+    cur.execute(select_Player_query)
+    emptynames = cur.fetchall()
+    for MLBID, PlayerName in emptynames:
+        try:
+            lastName, firstName = PlayerName.split(', ')
+            FullName = firstName + ' ' + lastName
+            MLBID = Get_MLB_ID(FullName)
+            cur2.execute (update_MLBID_query,(MLBID, PlayerName))
+            conn.commit()
+            print(f"MLBID: {MLBID}, PlayerName: {PlayerName}")
+        except (Error, Warning) as e:
+            print(f"MLBID: {MLBID}, PlayerName: {PlayerName}")
+            print("Didn't Work")
+except Error as e:
+    print(e)
+    sys.exit(1)
