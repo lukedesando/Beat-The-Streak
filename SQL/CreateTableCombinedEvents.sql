f26cbf9027885506d57d49b111ad000de1b39e79
lukedesando <Lukedesando@gmail.com>
2022-01-10 09:12:12 -0500

    Added HR

--- SQL/CreateTableCombinedEvents.sql
+++ SQL/CreateTableCombinedEvents.sql
@@ -1,4 +1,7 @@
-Create TABLE CombinedEvents(
+-- drop table PitcherVsBatterMatchups;
+
+
+Create TABLE PitcherVsBatterMatchups(
     pitcher_name tinytext,
     pitcher int,
     batter int,
@@ -7,10 +10,11 @@ Create TABLE CombinedEvents(
     AB smallINT,
     Hits smallINT,
     BA DOUBLE,
+    balls_in_play smallint,
     estimated_bacon DOUBLE,
     estimated_minus_actual_bacon DOUBLE,
     bacon double,
-    balls_in_play smallint
+    HR smallint
 )
 select
     pitcher,
@@ -19,44 +23,52 @@ select
     avg(estimated_ba_using_speedangle) as estimated_bacon,
     count(batter) as PA,
     SUM(babip_value)+count(CASE events when 'home_run' then 1 else null end) as Hits,
-    Count(CASE `description` when 'hit_into_play' then 1 else null end) as balls_in_play
+    Count(CASE `description` when 'hit_into_play' then 1 else null end) as balls_in_play,
+    Count(CASE `events` when 'home_run' then 1 else null end) as HR
 from
-    DatabaseBatterEvents
+    EventsBatter
 group by
     batter,
     pitcher;
 
-Update CombinedEvents c
+Update PitcherVsBatterMatchups c
 	Inner JOIN(
-select batter, pitcher, count(batter) as AB from DatabaseBatterEvents 
+select batter, pitcher, count(batter) as AB from EventsBatter 
 where `description` = 'hit_into_play' OR `description` = 'swinging_strike' OR `description` = 'swinging_strike_blocked' OR `description` = 'called_strike' or `description` = 'foul_tip'
 group by batter, pitcher) b
 	on c.batter = b.batter and c.pitcher = b.pitcher
 Set c.AB = b.AB;
 
-update CombinedEvents
+update PitcherVsBatterMatchups
 set bacon = Hits/nullif(balls_in_play,0);
 
-update CombinedEvents set BA = Hits/AB;
+update PitcherVsBatterMatchups set BA = Hits/AB;
 
-update CombinedEvents
+update PitcherVsBatterMatchups
 set estimated_minus_actual_bacon = estimated_bacon-bacon;
 
-ALTER TABLE `GameLogs`.`CombinedEvents`
+ALTER TABLE `GameLogs`.`PitcherVsBatterMatchups`
 CHANGE `pitcher` `pitcher` int(11) NULL DEFAULT NULL COMMENT '' AFTER `pitcher_name`,
 CHANGE `batter` `batter` int(11) NULL DEFAULT NULL COMMENT '' AFTER `pitcher`,
 CHANGE `batter_name` `batter_name` tinytext CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '' AFTER `batter`,
 CHANGE `PA` `PA` tinyint NULL DEFAULT NULL COMMENT '' AFTER `batter_name`,
 CHANGE `AB` `AB` tinyint NULL DEFAULT NULL COMMENT '' AFTER `PA`,
 CHANGE `Hits` `Hits` tinyint NULL DEFAULT NULL COMMENT '' AFTER `AB`,
-CHANGE `BA` `BA` double NULL DEFAULT NULL COMMENT '' AFTER `Hits`,
+CHANGE `HR` `HR` tinyint NULL DEFAULT NULL COMMENT '' AFTER `Hits`,
+CHANGE `BA` `BA` double NULL DEFAULT NULL COMMENT '' AFTER `HR`,
 CHANGE `balls_in_play` `balls_in_play` tinyint NULL DEFAULT NULL COMMENT '' AFTER `BA`,
 CHANGE `bacon` `bacon` double NULL DEFAULT NULL COMMENT '' AFTER `balls_in_play`,
 CHANGE `estimated_bacon` `estimated_bacon` double NULL DEFAULT NULL COMMENT '' AFTER `bacon`,
 CHANGE `estimated_minus_actual_bacon` `estimated_minus_actual_bacon` double NULL DEFAULT NULL COMMENT '' AFTER `estimated_bacon`;
 
+UPDATE PitcherVsBatterMatchups SET Hits=0 WHERE Hits is null;
+UPDATE PitcherVsBatterMatchups SET AB=0 WHERE AB is null;
+UPDATE PitcherVsBatterMatchups SET BA=0 WHERE BA is null;
+UPDATE PitcherVsBatterMatchups SET HR=0 WHERE HR is null;
+
+
 -- update
---     CombinedEvents C
+--     PitcherVsBatterMatchups C
 --     left join MLBIDs M on
 --     C.pitcher = M.PlayerID
 -- set
